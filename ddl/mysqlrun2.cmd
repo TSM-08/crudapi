33dc94963a783fbc67551187db96090fb3daf319
Serhii Tseluiko <serhii.tseluiko@thomsonreuters.com>
2021-10-20 21:57:35 +0300

    two

--- ddl/mysqlrun2.cmd
+++ ddl/mysqlrun2.cmd
@@ -6,7 +6,7 @@ echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 set path="C:\Program Files\MySQL\MySQL Server 8.0\bin";%path%
 @echo on
 copy db-build.sql + db-grant.sql + db-idata.sql mysql.tmp
-mysql.exe  -uroot -p22 --default-character-set=utf8mb4 -v --port 3306 < mysql.tmp
+mysql.exe  -uroot -p22 --default-character-set=utf8mb4 -v --port 13306 < mysql.tmp
 del mysql.tmp
 
 
---
+++ openshift-deployment/make_forward_conn.cmd
@@ -0,0 +1,2 @@
+login.cmd
+oc port-forward mysqldb-1-d4jv4 13306:3306
